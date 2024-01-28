04f433f2eccdd1792584b5ef70718476e7ad4909
Mohit Rajbhandari <rbmohit1283@gmail.com>
2024-01-28 23:17:09 +0545

    created components

---
+++ shoppingcart/src/components/productcard.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+type Props = {}
+
+type State = {}
+
+export default class productcard extends Component<Props, State> {
+  state = {}
+
+  render() {
+    return (
+      <div>productcard</div>
+    )
+  }
+}
---
+++ shoppingcart/src/components/productpage.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+type Props = {}
+
+type State = {}
+
+export default class productpage extends Component<Props, State> {
+  state = {}
+
+  render() {
+    return (
+      <div>productpage</div>
+    )
+  }
+}
