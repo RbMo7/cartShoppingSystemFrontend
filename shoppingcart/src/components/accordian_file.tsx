feefd874d5bddd92e2733248be3bc2ef43a3ade5
Mohit Rajbhandari <rbmohit1283@gmail.com>
2024-02-13 23:02:20 +0545

    added svg to accordian

--- shoppingcart/src/components/accordian_file.tsx
+++ shoppingcart/src/components/accordian_file.tsx
@@ -20,6 +20,9 @@ const Accordian_file = ({title, content}: Props) => {
       >
         <div className="items">
           <h3>{title}</h3>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" width="50" height="20">
+            <polygon points="20,50 70,25 70,75" fill="white" />
+          </svg>
         </div>
         <div className="data">
           <p>
